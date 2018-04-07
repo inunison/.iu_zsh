1cf830eeda0aabe4c9c0cc9e3ca67c52c4db688d
Jeff Hudson <jhudson@jhudson.ca>
2018-04-07 14:04:33 -0700

    Fix Google Cloud CLI path

--- post-zsh/google-cloud.sh
+++ post-zsh/google-cloud.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/jeffhudson/Downloads/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/jeffhudson/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]; then source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/jeffhudson/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/jeffhudson/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ]; then source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'; fi
